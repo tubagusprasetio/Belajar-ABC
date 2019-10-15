6c3abcfedc175d0e9728b4a04d04a5575c16433f
tubagusprasetio <tbprass@gmail.com>
2019-10-15 23:09:54 +0700

    First Commit

---
+++ Assets/Script/GameScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class GameScene : MonoBehaviour {
+
+	public LoadSceneMode loadMode = LoadSceneMode.Single;
+
+	public void LoadScene (string sceneName)
+	{
+		SceneManager.LoadScene (sceneName, loadMode);
+	}
+
+	public void LoadScene (int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex, loadMode);
+	}
+}
+	
